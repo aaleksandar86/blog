0458a46275146594767faa12a3c04d6f57b63128
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-05-07 19:14:08 +0200

    Create method added in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,21 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+
+    redirect_to article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
