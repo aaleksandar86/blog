089230183cef7a4d5be0d6fe7e982d0321ebd4c8
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-05-07 19:15:12 +0200

    Route for create added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # get 'articles/index'
-  get 'articles', to: 'articles#index'
+  get '/articles', to: 'articles#index'
+  post '/articles', to: 'articles#create'
   get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles/:id', to: 'articles#show', as: :article
+
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
