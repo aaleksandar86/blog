2c4bc576dd8fb473da4284214388f7f9dea69185
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-06-04 20:45:56 +0200

    Finished edit action implementation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,17 @@ class CommentsController < ApplicationController
     # @article = Article.find(params[article_id])
   end
 
+  def update
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+
+    if @comment.update(comment_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
 
   def comment_params
