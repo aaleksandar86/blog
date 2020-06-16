97bd7810c456fa1fd3012da5a743bb8a08806488
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-06-16 15:02:26 +0200

    Password attribute added to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'articles#index'
 
   resources :articles do
     resources :comments
   end
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
