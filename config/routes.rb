4fb5738c9299dd99a99933b9c0c7136393e38b88
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-06-25 18:25:32 +0200

    New, create and show actions implemented for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   root to: 'articles#index'
 
   resources :articles do
     resources :comments
   end
 
-  resources :users
-
-  get '/signup', to: 'users#new'
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
